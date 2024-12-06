c93ad93379114b634c7a1cbc4f2a6b43d315b5f6
BurciWolf <98662129+BurcisWolf@users.noreply.github.com>
2024-12-06 12:22:19 +0100

    Continued working on the DB, added Table PlayerScore

--- database.sql
+++ database.sql
@@ -17,3 +17,14 @@ CREATE TABLE Player
 
 ALTER TABLE Player
 	ADD CONSTRAINT pk_player PRIMARY KEY (playerID);
+
+CREATE TABLE PlayerScore
+	(
+		playerID int NOT NULL,
+		[level] int NOT NULL,
+		xp int NOT NULL,
+		[money] int NOT NULL,
+		reputation int NOT NULL,
+		health int NOT NULL,
+		vest int NOT NULL
+	);
